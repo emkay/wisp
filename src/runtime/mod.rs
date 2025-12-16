8dbf52207e2cffaf3fb43bd862e98737323111a0
Michael Matuzak <mmatuzak@gmail.com>
2025-12-16 07:05:55 -0800

    feat: audio works!

--- src/runtime/mod.rs
+++ src/runtime/mod.rs
@@ -1,3 +1,4 @@
+pub mod audio;
 pub mod graphics;
 pub mod input;
 pub mod tiled;
@@ -5,6 +6,7 @@ pub mod tiled;
 use crate::env::Env;
 
 pub fn load_runtime(env: &Env) {
+    audio::load_audio(env);
     graphics::load_graphics(env);
     input::load_input(env);
     tiled::load_tiled(env);
